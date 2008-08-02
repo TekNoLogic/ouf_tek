5607d5bdf99861fe8bf84f21010979e2eeed9a3b
Trond A Ekseth <troeks@gmail.com>
2008-08-02 16:12:35 +0200

    Added health bar.

--- layout.lua
+++ layout.lua
@@ -3,6 +3,7 @@
   without any conditions, unless such conditions are required by law.
 ---------------------------------------------------------------------------]]
 
+local texture = [[Interface\AddOns\oUF_Classic\textures\statusbar]]
 local height, width = 47, 260
 
 local menu = function(self)
@@ -16,6 +17,18 @@ local menu = function(self)
 	end
 end
 
+local OverrideUpdateHealth = function(self, event, bar, unit, min, max)
+	local color = self.colors.health[0]
+	bar:SetStatusBarColor(color.r, color.g, color.b)
+	bar.bg:SetVertexColor(color.r * .5, color.g * .5, color.b * .5)
+end
+
+local backdrop = {
+	bgFile = "Interface\\Tooltips\\UI-Tooltip-Background", tile = true, tileSize = 16,
+	edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 16,
+	insets = {left = 4, right = 4, top = 4, bottom = 4},
+}
+
 local func = function(settings, self, unit)
 	self.menu = menu
 
@@ -25,6 +38,29 @@ local func = function(settings, self, unit)
 	self:RegisterForClicks"anyup"
 	self:SetAttribute("*type2", "menu")
 
+	self:SetBackdrop(backdrop)
+	self:SetBackdropColor(0, 0, 0, 1)
+	self:SetBackdropBorderColor(.5, .5, .5, 1)
+
+	-- Health bar
+	local hp = CreateFrame"StatusBar"
+	hp:SetWidth(width - 90)
+	hp:SetHeight(14)
+	hp:SetStatusBarTexture(texture)
+
+	hp:SetParent(self)
+	hp:SetPoint("TOP", 0, -6)
+	hp:SetPoint("LEFT", 6, 0)
+
+	self.Health = hp
+	-- We have to override for now...
+	self.OverrideUpdateHealth = OverrideUpdateHealth
+
+	-- Health bar background
+	local hpbg = hp:CreateTexture(nil, "BORDER")
+	hpbg:SetAllPoints(hp)
+	hpbg:SetTexture(texture)
+	hp.bg = hpbg
 end
 
 oUF:RegisterStyle("Classic", setmetatable({
@@ -36,4 +72,3 @@ oUF:SetActiveStyle"Classic"
 
 local player = oUF:Spawn"player"
 player:SetPoint("CENTER", 0, -400)
-
