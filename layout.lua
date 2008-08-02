0de7c5dbf51e32ed059cc1d6e20afabfbe4a5520
Trond A Ekseth <troeks@gmail.com>
2008-08-02 18:03:46 +0200

    Add a new layout without the power bar. Move the frames.

--- layout.lua
+++ layout.lua
@@ -147,34 +147,6 @@ local func = function(settings, self, unit)
 	hpbg:SetTexture(texture)
 	hp.bg = hpbg
 
-	-- Power bar
-	local pp = CreateFrame"StatusBar"
-	pp:SetWidth(width - 90)
-	pp:SetHeight(14)
-	pp:SetStatusBarTexture(texture)
-
-	pp:SetParent(self)
-	pp:SetPoint("BOTTOM", 0, 8)
-	pp:SetPoint("LEFT", 8, 0)
-
-	self.Power = pp
-
-	-- Power bar background
-	local ppbg = pp:CreateTexture(nil, "BORDER")
-	ppbg:SetAllPoints(pp)
-	ppbg:SetTexture(texture)
-	pp.bg = ppbg
-
-	local ppp = hp:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-	ppp:SetPoint("LEFT", pp, "RIGHT", 2, 0)
-	ppp:SetPoint("RIGHT", self, -6, 0)
-	ppp:SetJustifyH"CENTER"
-	ppp:SetFont(GameFontNormal:GetFont(), 10)
-	ppp:SetTextColor(1, 1, 1)
-
-	pp.value = ppp
-	self.PostUpdatePower = PostUpdatePower
-
 	-- Unit name
 	local name = hp:CreateFontString(nil, "OVERLAY", "GameFontNormal")
 	name:SetPoint("LEFT", 2, -1)
@@ -185,21 +157,51 @@ local func = function(settings, self, unit)
 
 	self.Name = name
 
-	-- Info string
-	local info = pp:CreateFontString(nil, "OVERLAY", "GameFontNormal")
-	info:SetPoint("LEFT", 2, -1)
-	info:SetPoint("RIGHT", -2, 0)
-	info:SetJustifyH"LEFT"
-	info:SetFont(GameFontNormal:GetFont(), 11)
-	info:SetTextColor(1, 1, 1)
+	if(settings.size ~= 'small') then
+		-- Power bar
+		local pp = CreateFrame"StatusBar"
+		pp:SetWidth(width - 90)
+		pp:SetHeight(14)
+		pp:SetStatusBarTexture(texture)
 
-	self.Info = info
-	self.UNIT_LEVEL = updateInfoString
-	self:RegisterEvent"UNIT_LEVEL"
+		pp:SetParent(self)
+		pp:SetPoint("BOTTOM", 0, 8)
+		pp:SetPoint("LEFT", 8, 0)
 
-	if(unit == "pet") then
-		self.UNIT_HAPPINESS = updateInfoString
-		self:RegisterEvent"UNIT_HAPPINESS"
+		self.Power = pp
+
+		-- Power bar background
+		local ppbg = pp:CreateTexture(nil, "BORDER")
+		ppbg:SetAllPoints(pp)
+		ppbg:SetTexture(texture)
+		pp.bg = ppbg
+
+		local ppp = hp:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+		ppp:SetPoint("LEFT", pp, "RIGHT", 2, 0)
+		ppp:SetPoint("RIGHT", self, -6, 0)
+		ppp:SetJustifyH"CENTER"
+		ppp:SetFont(GameFontNormal:GetFont(), 10)
+		ppp:SetTextColor(1, 1, 1)
+
+		pp.value = ppp
+		self.PostUpdatePower = PostUpdatePower
+
+		-- Info string
+		local info = pp:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+		info:SetPoint("LEFT", 2, -1)
+		info:SetPoint("RIGHT", -2, 0)
+		info:SetJustifyH"LEFT"
+		info:SetFont(GameFontNormal:GetFont(), 11)
+		info:SetTextColor(1, 1, 1)
+
+		self.Info = info
+		self.UNIT_LEVEL = updateInfoString
+		self:RegisterEvent"UNIT_LEVEL"
+
+		if(unit == "pet") then
+			self.UNIT_HAPPINESS = updateInfoString
+			self:RegisterEvent"UNIT_HAPPINESS"
+		end
 	end
 
 	-- Buffs
@@ -224,6 +226,14 @@ local func = function(settings, self, unit)
 	debuffs.num = math.floor(width / debuffs.size + .5)
 
 	self.Debuffs = debuffs
+
+	-- Range fading on party
+	if(not unit) then
+		self.Range = true
+		self.inRangeAlpha = 1
+		self.outsideRangeAlpha = .5
+	end
+
 end
 
 oUF:RegisterStyle("Classic", setmetatable({
@@ -231,12 +241,18 @@ oUF:RegisterStyle("Classic", setmetatable({
 	["initial-height"] = height,
 }, {__call = func}))
 
+oUF:RegisterStyle("Classic - Small", setmetatable({
+	["initial-width"] = width,
+	["initial-height"] = height - 16,
+	["size"] = 'small',
+}, {__call = func}))
+
 -- hack to get our level information updated.
 oUF:RegisterSubTypeMapping"UNIT_LEVEL"
 oUF:SetActiveStyle"Classic"
 
 local player = oUF:Spawn"player"
-player:SetPoint("CENTER", 0, -400)
+player:SetPoint("CENTER", -200, -381)
 
 local player = oUF:Spawn"target"
-player:SetPoint("CENTER", 0, -300)
+player:SetPoint("CENTER", 200, -381)
